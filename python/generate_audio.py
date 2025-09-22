74b58b7488b79265723e3cfb105ebe84b3cc3eed
Vineet <c-vverma@dovercorp.com>
2025-09-22 14:51:15 +0530

    Updated audio script to use eleven labs

--- python/generate_audio.py
+++ python/generate_audio.py
@@ -1,23 +1,55 @@
+# import os
+# from dotenv import load_dotenv
+# from murf import Murf
+
+# load_dotenv()
+
+# client = Murf(api_key=os.getenv("MURFAI_API_KEY"))
+
+# def generate_audio(fact):
+#     response = client.text_to_speech.generate(
+#         text=fact,
+#         voice_id="en-US-natalie",
+#         style="Promo",
+#         pitch=-25,
+#         rate=0,
+#     )
+
+#     print(response.audio_file)
+
+
+# generate_audio(
+#     f"""Did you know?
+#     There are more stars in the observable universe than all the grains of sand on every beach on Earth? Let that staggering number sink in! Wild, right?
+#     Like, share, and subscribe for more!
+#     """
+# )
+
 import os
+from datetime import date
 from dotenv import load_dotenv
-from murf import Murf
+from elevenlabs.client import ElevenLabs
+from elevenlabs import play
 
 load_dotenv()
 
-client = Murf(api_key=os.getenv("MURFAI_API_KEY"))
+output_path = f"""/Users/vineetverma/Desktop/Learnings/one-fact-a-day/python/assets/voice"""
 
-def generate_audio(fact):
-    response = client.text_to_speech.generate(
-        text=fact,
-        voice_id="en-US-natalie",
-        style="Promo",
-        pitch=-25,
-        rate=25,
-    )
+client = ElevenLabs(api_key=os.getenv("ELEVENLABS_API_KEY"))
 
-    print(response.audio_file)
-
-
-generate_audio(
-    "Did you know? There are more stars in the observable universe than all the grains of sand on every beach on Earth? Let that staggering number sink in! Wild, right? Like, share, and subscribe for more!"
+audio = client.text_to_speech.convert(
+    text="Did you know? There are more stars in the observable universe than all the grains of sand on every beach on Earth? Let that staggering number sink in! Wild, right?... Like, share, and subscribe for more!",
+    # voice_id="nPczCjzI2devNBz1zQrb", default,
+    voice_id="zgqefOY5FPQ3bB7OZTVR",
+    model_id="eleven_multilingual_v2",
+    output_format="mp3_44100_128",
 )
+
+# Save audio file
+with open(f"{output_path}/{date.today()}.mp3", "wb") as f:
+    for chunk in audio:
+        f.write(chunk)
+
+# Play the audio
+with open("did_you_know.mp3", "rb") as f:
+    play(f.read())
--- python/generate_script.py
+++ python/generate_script.py
@@ -9,7 +9,7 @@ client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 def generate_script():
   response = client.models.generate_content(
       model="gemini-2.5-flash",
-      contents="Create a short, engaging script (15 seconds when spoken) for a YouTube Short video. The script MUST start with the exact words: 'Did you know?'Requirements: -Use a real, verifiable fact. -Make it surprising, and conversational (not academic).  - Add emotional or sensory words to make it engaging. -End with a punchy closing line that invites viewers to like, share, and subscribe. - Keep language simple and natural, as if speaking to a general audience. Return ONLY the script text, nothing else.",
+      contents="Create a short, engaging script (15 seconds when spoken) for a YouTube Short video. The script MUST start with the exact words: 'Did you know? 'Requirements: - Use a real, verifiable fact. - Make it surprising, and conversational (not academic). - Add emotional or sensory words to make it engaging. - End with a punchy closing line that invites viewers to like, share, and subscribe. - Keep language simple and natural, as if speaking to a general audience. Return ONLY the script text, nothing else.",
   )
   print(response.text)
   
